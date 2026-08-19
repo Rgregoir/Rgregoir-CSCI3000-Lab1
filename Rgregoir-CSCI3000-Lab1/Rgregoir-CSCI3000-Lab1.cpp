d872ac3657b6431620398e1b3f9a8c65b29b0c7e
Richard Gregoire <rgregoir@aum.edu>
2026-08-19 16:19:55 -0500

    Final mini-task update

--- Rgregoir-CSCI3000-Lab1/Rgregoir-CSCI3000-Lab1.cpp
+++ Rgregoir-CSCI3000-Lab1/Rgregoir-CSCI3000-Lab1.cpp
@@ -8,5 +8,6 @@ int main()
     cout << "Hello from Richard Gregoire! This is my first Git commit\n";
     cout << "Git makes collaboration easy!" << endl;
     cout << "Changes made by Haiden!\n";
+    cout << "Lab1 completed on 8/19/2026" << endl;
 }
 
